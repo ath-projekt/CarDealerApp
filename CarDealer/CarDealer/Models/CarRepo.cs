5e996c1fff295ed3f003c59c77d1c6e1e2a4c5e9
Dziki Przemyslaw <p.dziki1984@gmail.com>
2019-06-02 21:15:41 +0200

    added CarRepo

---
+++ CarDealer/CarDealer/Models/CarRepo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealer.Models
+{
+    public class CarRepo : ICarRepo
+    {
+        public void AddCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car GetCar(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Car> GetCars()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
