7d2777a959cb8243d4a5b47e433b3eeaa80cc3d4
Dziki Przemyslaw <p.dziki1984@gmail.com>
2019-06-02 21:12:33 +0200

    added ICarRepo

---
+++ CarDealer/CarDealer/Models/ICarRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealer.Models
+{
+    public interface ICarRepo
+    {
+        IEnumerable<Car> GetCars();
+        Car GetCar(int carId);
+
+        void AddCar(Car car);
+        void EditCar(Car car);
+        void DeleteCar(Car car);
+
+    }
+}
