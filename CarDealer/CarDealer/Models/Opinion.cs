19b2ad68d2bdffc70997046a73df673288a804b1
Dziki Przemyslaw <p.dziki1984@gmail.com>
2019-06-03 10:18:49 +0200

    added Opinion, OpinionRepo, I OpinionRepo

---
+++ CarDealer/CarDealer/Models/Opinion.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealer.Models
+{
+    public class Opinion
+    {
+        [BindNever]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nazwa użytkownika jest wymagana")]
+        [StringLength(100, ErrorMessage = "Nazwa użytkownika jest za długa")]
+        public string UserName { get; set; }
+
+
+        [Required(ErrorMessage = "Adres email jest wymagany")]
+        [StringLength(100, ErrorMessage ="Adres email jest za długi")]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
+            ErrorMessage = "To nie jest adres email")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Wiadomość jest wymagana")]
+        [StringLength(5000, ErrorMessage = "Wiadomość jest za długa")]
+        public string Message { get; set; }
+
+        public bool RequireAnswer { get; set; }
+
+
+    }
+}
---
+++ CarDealer/CarDealer/Models/OpinionRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealer.Models
+{
+    public class OpinionRepo : IOpinionRepo
+    {
+        private readonly AppDBContext _appDBContext;
+
+        public OpinionRepo(AppDBContext appDBContext)
+        {
+            _appDBContext = appDBContext;
+        }
+
+        public void AddOpinion(Opinion opinion)
+        {
+            _appDBContext.Opinions.Add(opinion);
+            _appDBContext.SaveChanges();
+        }
+    }
+}
