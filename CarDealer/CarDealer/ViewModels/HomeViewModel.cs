3d57ff52b6ae0698136cfb02964847abc41e98d0
Piotr Rybka <piotr.rybka82@gmail.com>
2019-06-23 08:14:39 +0200

    Dodano view-modele

--- CarDealer/CarDealer/ViewModels/HomeViewModel.cs
+++ CarDealer/CarDealer/ViewModels/HomeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarDealer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,9 @@ namespace CarDealer.ViewModels
 {
     public class HomeViewModel
     {
+        public string Title { get; set; }
+        public List<Car> Cars { get; set; }
+
+
     }
 }
--- CarDealer/CarDealer/ViewModels/LoginViewModel.cs
+++ CarDealer/CarDealer/ViewModels/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace CarDealer.ViewModels
 {
     public class LoginViewModel
     {
+        [Required]
+        [Display(Name = "Nazwa użytkownika")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Hasło")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
     }
 }
