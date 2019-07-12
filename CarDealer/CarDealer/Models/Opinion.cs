f8d3b860865e3f557df6369acaafc30697711f09
Piotr Rybka <piotr.rybka82@gmail.com>
2019-07-13 00:25:44 +0200

    Poprawki widoku Opinion. Wymagana korekta dodawania opinii

--- CarDealer/CarDealer/Models/Opinion.cs
+++ CarDealer/CarDealer/Models/Opinion.cs
@@ -2,14 +2,18 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace CarDealer.Models
 {
     public class Opinion
     {
         [BindNever]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Nazwa użytkownika jest wymagana")]
